71d59b5825882606fef452c9328abf939ed1156a
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-08-15 17:20:41 +1000

    Add a function to play a round and announce the result from the player's point of view

--- js/script.js
+++ js/script.js
@@ -1,9 +1,59 @@
 const choiceList= ['Paper', 'Scissors', 'Rock']
 
 // Randomly return paper, rock or scissors as computer's choice
-function getComputerChoice(choiceList){
+function getComputerChoice(){
     randomNumber = Math.floor(Math.random() * choiceList.length); 
     return choiceList[randomNumber]
 }
 
-let computerChoice = getComputerChoice(choiceList)
+
+/* A function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - the playerSelection 
+and computerSelection - and then return a string that declares the
+ winner of the round like so: "You Lose! Paper beats Rock"
+ */
+
+ function playRound(playerSelection, computerSelection) {
+    let playerSelectionUpper = playerSelection.toUpperCase()
+    let computerSelectionUpper = computerSelection.toUpperCase()
+    let playerSelectionNumber = 0
+    let computerSelectionNumber = 0
+
+    switch(playerSelectionUpper) {
+        case "ROCK":
+            playerSelectionNumber = 3
+            break
+        case "SISSORS":
+            playerSelectionNumber = 2
+            break
+        case "PAPER":
+            playerSelectionNumber = 1
+            break
+    }
+    
+    switch(computerSelectionUpper) {
+        case "ROCK":
+            computerSelectionNumber = 3
+            break
+        case "SISSORS":
+            computerSelectionNumber = 2
+            break
+        case "PAPER":
+            computerSelectionNumber = 1
+            break
+    }
+
+    let playResult = computerSelectionNumber - playerSelectionNumber
+
+    if (playResult > 1 || playResult === -2){ 
+        console.log("You Lose")
+    } else if (playResult === 0) {
+        console.log("Draw")
+    } else {
+        console.log("You Win")
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
