f803fbe64f785507909e674cc681c2ce08f47a2e
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-08-16 15:40:36 +1000

    Update the code style to use single quote for strings and template strings

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const choiceList= ['Paper', 'Scissors', 'Rock']
+const choiceList= ['Paper', 'Scissors', 'Rock'];
 
 // Randomly return paper, rock or scissors as computer's choice
 function getComputerChoice(){
@@ -9,11 +9,11 @@ function getComputerChoice(){
 // Convert paper, rock and scissors to numbers for easier comparison 
 function convertChoice(choice){
     switch(choice){
-        case "PAPER":
+        case 'PAPER':
             return 1
-        case "SCISSORS":
+        case 'SCISSORS':
             return 2
-        case "ROCK":
+        case 'ROCK':
             return 3
     } 
 }
@@ -40,11 +40,11 @@ function playRound(playerSelection, computerSelection) {
 
     // Evaluate the differences to determine who wins and print out the round
     if (playResult > 1 || playResult === -2){ 
-        console.log("You Lose! " + computerSelection + " Beat " + playerSelection)
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
     } else if (playResult === 0) {
-        console.log("Draw! You both play " + computerSelection)
+        console.log(`Draw! You both play ${computerSelection}`)
     } else {
-        console.log("You Win! " + playerSelection + " Beat " + computerSelection)
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
         ++playerScore
     }
     return playerScore
@@ -54,16 +54,14 @@ function playRound(playerSelection, computerSelection) {
 function game(){
     score = 0
     for (let i = 0; i < 2; i++){
-        const playerSelection = prompt("Choice paper, scissors or rock")
+        const playerSelection = prompt('Choice paper, scissors or rock')
         const computerSelection = getComputerChoice()
         playScore = playRound(playerSelection, computerSelection)
         score = score + playScore
 
     }
-    console.log("You won " + score + " games!")
+    console.log(`You won ${score} games!`)
 }
-const playerSelection = prompt("Choice paper, scissors or rock")
-const computerSelection = getComputerChoice()
-playScore = playRound(playerSelection, computerSelection)
 
-// game()
+
+game()
